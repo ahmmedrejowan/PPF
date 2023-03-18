35c383194bda8fb50574dc681d24a96a4288d713
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-03-18 18:05:22 +0600

    Function and UI Changes

---
+++ app/src/main/java/com/rejowan/ppfdemo/SecurityQuestions.java
@@ -0,0 +1,14 @@
+package com.rejowan.ppfdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecurityQuestions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_security_questions);
+    }
+}
---
+++ app/src/main/java/com/rejowan/ppfdemo/Unlocked.java
@@ -0,0 +1,14 @@
+package com.rejowan.ppfdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Unlocked extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unlocked);
+    }
+}

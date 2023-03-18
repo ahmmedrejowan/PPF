bb886ef1cbc5a9482a8884720384294f465478d9
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-03-18 19:09:39 +0600

    Function and UI Changes

--- app/src/main/java/com/rejowan/ppfdemo/MainActivity.java
+++ app/src/main/java/com/rejowan/ppfdemo/MainActivity.java
@@ -1,18 +1,11 @@
 package com.rejowan.ppfdemo;
 
-import static com.rejowan.ppf.PatternLockView.stringToPattern;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.rejowan.ppf.PatternLockView;
 import com.rejowan.ppfdemo.databinding.ActivityMainBinding;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,65 +18,73 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.pin.setOnClickListener(v -> startActivity(new Intent(this, Pin.class)));
 
-        binding.pinLockView.attachIndicatorDots(binding.indicatorDots);
-        binding.indicatorDots.setCount(binding.pinLockView.getPinLength());
+        binding.pattern.setOnClickListener(v -> startActivity(new Intent(this, Pattern.class)));
 
+        binding.reset.setOnClickListener(v -> startActivity(new Intent(this, SecurityQuestions.class)));
 
-
-
-
-        binding.patternLockView.setPattern(PatternLockView.PatternViewMode.AUTO_DRAW, stringToPattern(binding.patternLockView, "01234"));
-
-        binding.patternLockView.addPatternLockListener(new PatternLockView.PatternLockViewListener() {
-            @Override
-            public void onStarted() {
-
-            }
-
-            @Override
-            public void onProgress(List<PatternLockView.Dot> progressPattern) {
-
-            }
-
-            @Override
-            public void onComplete(List<PatternLockView.Dot> pattern) {
-
-                if (PatternLockView.patternToString(binding.patternLockView, pattern).equals("0367")) {
-
-                    Log.e("TAG", "success: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-
-                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
-
-                    new Handler().postDelayed(() -> Toast.makeText(MainActivity.this, "Logged In", Toast.LENGTH_SHORT).show(), 1000);
+        binding.finger.setOnClickListener(v -> startActivity(new Intent(this, FingerPrint.class)));
 
 
-                } else {
-                    Log.e("TAG", "failed: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-
-                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
-
-                    new Handler().postDelayed(() -> {
-
-                        Toast.makeText(MainActivity.this, "Wrong Pattern", Toast.LENGTH_SHORT).show();
-
-                        binding.patternLockView.clearPattern();
-
-                    }, 1000);
-
-
-                }
-
-                Log.e("TAG", "onComplete: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-                Log.e("TAG", "onComplete: " + pattern);
-
-            }
-
-            @Override
-            public void onCleared() {
-
-            }
-        });
+//        binding.pinLockView.attachIndicatorDots(binding.indicatorDots);
+//        binding.indicatorDots.setCount(binding.pinLockView.getPinLength());
+//
+//
+//
+//
+//
+//        binding.patternLockView.setPattern(PatternLockView.PatternViewMode.AUTO_DRAW, stringToPattern(binding.patternLockView, "01234"));
+//
+//        binding.patternLockView.addPatternLockListener(new PatternLockView.PatternLockViewListener() {
+//            @Override
+//            public void onStarted() {
+//
+//            }
+//
+//            @Override
+//            public void onProgress(List<PatternLockView.Dot> progressPattern) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete(List<PatternLockView.Dot> pattern) {
+//
+//                if (PatternLockView.patternToString(binding.patternLockView, pattern).equals("0367")) {
+//
+//                    Log.e("TAG", "success: " + PatternLockView.patternToString(binding.patternLockView, pattern));
+//
+//                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
+//
+//                    new Handler().postDelayed(() -> Toast.makeText(MainActivity.this, "Logged In", Toast.LENGTH_SHORT).show(), 1000);
+//
+//
+//                } else {
+//                    Log.e("TAG", "failed: " + PatternLockView.patternToString(binding.patternLockView, pattern));
+//
+//                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+//
+//                    new Handler().postDelayed(() -> {
+//
+//                        Toast.makeText(MainActivity.this, "Wrong Pattern", Toast.LENGTH_SHORT).show();
+//
+//                        binding.patternLockView.clearPattern();
+//
+//                    }, 1000);
+//
+//
+//                }
+//
+//                Log.e("TAG", "onComplete: " + PatternLockView.patternToString(binding.patternLockView, pattern));
+//                Log.e("TAG", "onComplete: " + pattern);
+//
+//            }
+//
+//            @Override
+//            public void onCleared() {
+//
+//            }
+//        });
 
 
     }

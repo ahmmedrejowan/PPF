dcbe03201a092d8ddf20542fe3939137da053839
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-03-21 01:31:17 +0600

    Setting Up Security Questions

--- app/src/main/java/com/rejowan/ppfdemo/SecurityQuestions.java
+++ app/src/main/java/com/rejowan/ppfdemo/SecurityQuestions.java
@@ -1,9 +1,12 @@
 package com.rejowan.ppfdemo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.rejowan.ppf.SQUtils;
 import com.rejowan.ppfdemo.databinding.ActivitySecurityQuestionsBinding;
@@ -13,6 +16,9 @@ import java.util.ArrayList;
 public class SecurityQuestions extends AppCompatActivity {
 
     ActivitySecurityQuestionsBinding binding;
+    SQUtils sqUtils;
+
+    ArrayList<String> questions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,31 +26,140 @@ public class SecurityQuestions extends AppCompatActivity {
         binding = ActivitySecurityQuestionsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        sqUtils = new SQUtils(this);
 
-        SQUtils sqUtils = new SQUtils(this);
+        SetupQuestions();
 
-        Log.e("TAG", "onCreate 1: " + sqUtils.getSecurityQuestions());
+        SetupQA();
 
+        SetupVerify();
 
-        ArrayList<String> questions = new ArrayList<>();
-        questions.add("What is your mother's maiden name?");
-        questions.add("What is your pet's name?");
-        questions.add("What is your favorite color?");
-        questions.add("What is your favorite food?");
-        questions.add("What is your favorite movie?");
-        questions.add("What is your favorite book?");
-        questions.add("What is your favorite song?");
+    }
 
-        sqUtils.addSecurityQuestions(questions);
+    private void SetupQuestions() {
 
+        // adding questions to the list
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("What is your mother's maiden name?");
+        stringArrayList.add("What is your pet's name?");
+        stringArrayList.add("What is your favorite color?");
+        stringArrayList.add("What is your favorite food?");
+        stringArrayList.add("What is your favorite movie?");
+        stringArrayList.add("What is your favorite book?");
+        stringArrayList.add("What is your favorite song?");
+        sqUtils.setAllSQList(stringArrayList);
 
-
-        Log.e("TAG", "onCreate 2: " + sqUtils.getSecurityQuestions());
-
-
-
-
+        questions.add("Please select a question");
+        questions.addAll(sqUtils.getAllSQList());
 
 
     }
+
+    private void SetupQA() {
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, questions);
+        spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_item_drop);
+        binding.spinner1.setAdapter(spinnerArrayAdapter);
+
+
+        binding.spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    // remove the selected item from the list and set adapter for spinner 2
+                    ArrayList<String> questions2 = new ArrayList<>(questions);
+                    questions2.remove(position);
+                    ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<>(SecurityQuestions.this, R.layout.spinner_item, questions2);
+                    spinnerArrayAdapter2.setDropDownViewResource(R.layout.spinner_item_drop);
+                    binding.spinner2.setAdapter(spinnerArrayAdapter2);
+
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+        binding.submit.setOnClickListener(v -> {
+
+            // get the selected items from the spinners
+
+            if (binding.spinner1.getSelectedItemPosition() == 0) {
+                Toast.makeText(this, "Please select a question", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (binding.spinner2.getSelectedItemPosition() == 0) {
+                Toast.makeText(this, "Please select a question", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            String question1 = binding.spinner1.getSelectedItem().toString();
+            String question2 = binding.spinner2.getSelectedItem().toString();
+
+            String answer1 = binding.answer1.getText().toString();
+            String answer2 = binding.answer2.getText().toString();
+
+            if (answer1.isEmpty()) {
+                binding.answer1.setError("Please enter an answer");
+            } else if (answer2.isEmpty()) {
+                binding.answer2.setError("Please enter an answer");
+            } else {
+
+                ArrayList<String> selectedQuestions = new ArrayList<>();
+                selectedQuestions.add(question1);
+                selectedQuestions.add(question2);
+                sqUtils.setSelectedSQList(selectedQuestions);
+
+                sqUtils.setSQAnswer(question1, answer1);
+                sqUtils.setSQAnswer(question2, answer2);
+
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+                SetupVerify();
+
+            }
+
+
+        });
+    }
+
+    private void SetupVerify() {
+
+        ArrayList<String> userSelectedQuestions = sqUtils.getSelectedSQList();
+
+        binding.q1.setText(userSelectedQuestions.get(0));
+        binding.q2.setText(userSelectedQuestions.get(1));
+
+
+        binding.submit2.setOnClickListener(v -> {
+
+            String answer1 = binding.answer3.getText().toString();
+            String answer2 = binding.answer4.getText().toString();
+
+            if (answer1.isEmpty()) {
+                binding.answer1.setError("Please enter an answer");
+            } else if (answer2.isEmpty()) {
+                binding.answer2.setError("Please enter an answer");
+            } else {
+
+                if (sqUtils.checkSQAnswer(userSelectedQuestions.get(0), answer1) && sqUtils.checkSQAnswer(userSelectedQuestions.get(1), answer2)) {
+                    Toast.makeText(this, "Verified", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(this, "Wrong answers", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+
+        });
+
+    }
+
+
 }
--- ppf/src/main/java/com/rejowan/ppf/SQUtils.java
+++ ppf/src/main/java/com/rejowan/ppf/SQUtils.java
@@ -3,40 +3,86 @@ package com.rejowan.ppf;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.text.TextUtils;
+import android.util.Log;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SQUtils {
 
-    private final String PREF_NAME = "ppf_sq_pref";
-    private final Context context;
     private final SharedPreferences preferences;
     private final SharedPreferences.Editor editor;
     private final String SQ_QUESTIONS = "ppf_sq_ques_pref";
+    public static final String SQ_SELECTED = "ppf_sq_ques_pref_selected";
 
 
     public SQUtils(Context context) {
-        this.context = context;
+        String PREF_NAME = "ppf_sq_pref";
         preferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         editor = preferences.edit();
 
     }
 
-    public ArrayList<String> getSecurityQuestions() {
+    public ArrayList<String> getAllSQList() {
         return new ArrayList<>(Arrays.asList(TextUtils.split(this.preferences.getString(SQ_QUESTIONS, ""), "‚‗‚")));
     }
 
 
-    public void addSecurityQuestions(ArrayList<String> arrayList) {
-        ArrayList<String> securityQuestions = getSecurityQuestions();
+    public void setAllSQList(ArrayList<String> arrayList) {
+        ArrayList<String> securityQuestions = getAllSQList();
         for (String s : arrayList) {
             if (!securityQuestions.contains(s)) {
                 securityQuestions.add(s);
             }
         }
-        this.preferences.edit().putString(SQ_QUESTIONS, TextUtils.join("‚‗‚", (String[]) securityQuestions.toArray(new String[securityQuestions.size()]))).apply();
+        this.editor.putString(SQ_QUESTIONS, TextUtils.join("‚‗‚", (String[]) securityQuestions.toArray(new String[securityQuestions.size()]))).apply();
     }
+
+    public ArrayList<String> getSelectedSQList() {
+        return new ArrayList<>(Arrays.asList(TextUtils.split(this.preferences.getString(SQ_SELECTED, ""), "‚‗‚")));
+    }
+
+    public void setSelectedSQList(ArrayList<String> arrayList) {
+        this.editor.putString(SQ_SELECTED, "").apply();
+        this.editor.putString(SQ_SELECTED, TextUtils.join("‚‗‚", (String[]) arrayList.toArray(new String[arrayList.size()]))).apply();
+    }
+
+
+    public void setSQAnswer(String question, String answer) {
+        this.editor.putString("answer:" + question, stringToSHA256(answer)).apply();
+    }
+
+    public boolean checkSQAnswer(String question, String answer) {
+        return this.preferences.getString("answer:" + question, "").equals(stringToSHA256(answer));
+    }
+
+    public static String stringToSHA256(String string) {
+        try {
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e1) {
+                Log.e("TAG", "stringToSHA256: ", e1);
+            }
+            assert digest != null;
+            digest.reset();
+            return bin2hex(digest.digest(string.getBytes()));
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
+    private static String bin2hex(byte[] data) {
+        StringBuilder hex = new StringBuilder(data.length * 2);
+        for (byte b : data)
+            hex.append(String.format("%02x", b & 0xFF));
+        return hex.toString();
+    }
+
+
+
 
 
 }

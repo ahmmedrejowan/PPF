90fa6a45bc5a87828f4de6cb5bbb8cc3448553ea
ahmmedrejowan <ahmmedrejowan@gmail.com>
2023-03-21 01:54:09 +0600

    UI and Functions Update

--- app/src/main/java/com/rejowan/ppfdemo/MainActivity.java
+++ app/src/main/java/com/rejowan/ppfdemo/MainActivity.java
@@ -27,64 +27,6 @@ public class MainActivity extends AppCompatActivity {
         binding.finger.setOnClickListener(v -> startActivity(new Intent(this, FingerPrint.class)));
 
 
-//        binding.pinLockView.attachIndicatorDots(binding.indicatorDots);
-//        binding.indicatorDots.setCount(binding.pinLockView.getPinLength());
-//
-//
-//
-//
-//
-//        binding.patternLockView.setPattern(PatternLockView.PatternViewMode.AUTO_DRAW, stringToPattern(binding.patternLockView, "01234"));
-//
-//        binding.patternLockView.addPatternLockListener(new PatternLockView.PatternLockViewListener() {
-//            @Override
-//            public void onStarted() {
-//
-//            }
-//
-//            @Override
-//            public void onProgress(List<PatternLockView.Dot> progressPattern) {
-//
-//            }
-//
-//            @Override
-//            public void onComplete(List<PatternLockView.Dot> pattern) {
-//
-//                if (PatternLockView.patternToString(binding.patternLockView, pattern).equals("0367")) {
-//
-//                    Log.e("TAG", "success: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-//
-//                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
-//
-//                    new Handler().postDelayed(() -> Toast.makeText(MainActivity.this, "Logged In", Toast.LENGTH_SHORT).show(), 1000);
-//
-//
-//                } else {
-//                    Log.e("TAG", "failed: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-//
-//                    binding.patternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
-//
-//                    new Handler().postDelayed(() -> {
-//
-//                        Toast.makeText(MainActivity.this, "Wrong Pattern", Toast.LENGTH_SHORT).show();
-//
-//                        binding.patternLockView.clearPattern();
-//
-//                    }, 1000);
-//
-//
-//                }
-//
-//                Log.e("TAG", "onComplete: " + PatternLockView.patternToString(binding.patternLockView, pattern));
-//                Log.e("TAG", "onComplete: " + pattern);
-//
-//            }
-//
-//            @Override
-//            public void onCleared() {
-//
-//            }
-//        });
 
 
     }
--- ppf/src/main/java/com/rejowan/ppf/SQUtils.java
+++ ppf/src/main/java/com/rejowan/ppf/SQUtils.java
@@ -12,10 +12,10 @@ import java.util.Arrays;
 
 public class SQUtils {
 
+    public static final String SQ_SELECTED = "ppf_sq_ques_pref_selected";
     private final SharedPreferences preferences;
     private final SharedPreferences.Editor editor;
     private final String SQ_QUESTIONS = "ppf_sq_ques_pref";
-    public static final String SQ_SELECTED = "ppf_sq_ques_pref_selected";
 
 
     public SQUtils(Context context) {
@@ -25,10 +25,32 @@ public class SQUtils {
 
     }
 
+    private String bin2hex(byte[] data) {
+        StringBuilder hex = new StringBuilder(data.length * 2);
+        for (byte b : data)
+            hex.append(String.format("%02x", b & 0xFF));
+        return hex.toString();
+    }
+
+    private String stringToSHA256(String string) {
+        try {
+            MessageDigest digest = null;
+            try {
+                digest = MessageDigest.getInstance("SHA-256");
+            } catch (NoSuchAlgorithmException e1) {
+                Log.e("TAG", "stringToSHA256: ", e1);
+            }
+            assert digest != null;
+            digest.reset();
+            return bin2hex(digest.digest(string.getBytes()));
+        } catch (Exception ignored) {
+            return null;
+        }
+    }
+
     public ArrayList<String> getAllSQList() {
         return new ArrayList<>(Arrays.asList(TextUtils.split(this.preferences.getString(SQ_QUESTIONS, ""), "‚‗‚")));
     }
-
 
     public void setAllSQList(ArrayList<String> arrayList) {
         ArrayList<String> securityQuestions = getAllSQList();
@@ -49,7 +71,6 @@ public class SQUtils {
         this.editor.putString(SQ_SELECTED, TextUtils.join("‚‗‚", (String[]) arrayList.toArray(new String[arrayList.size()]))).apply();
     }
 
-
     public void setSQAnswer(String question, String answer) {
         this.editor.putString("answer:" + question, stringToSHA256(answer)).apply();
     }
@@ -57,30 +78,6 @@ public class SQUtils {
     public boolean checkSQAnswer(String question, String answer) {
         return this.preferences.getString("answer:" + question, "").equals(stringToSHA256(answer));
     }
-
-    public static String stringToSHA256(String string) {
-        try {
-            MessageDigest digest = null;
-            try {
-                digest = MessageDigest.getInstance("SHA-256");
-            } catch (NoSuchAlgorithmException e1) {
-                Log.e("TAG", "stringToSHA256: ", e1);
-            }
-            assert digest != null;
-            digest.reset();
-            return bin2hex(digest.digest(string.getBytes()));
-        } catch (Exception ignored) {
-            return null;
-        }
-    }
-
-    private static String bin2hex(byte[] data) {
-        StringBuilder hex = new StringBuilder(data.length * 2);
-        for (byte b : data)
-            hex.append(String.format("%02x", b & 0xFF));
-        return hex.toString();
-    }
-
 
 
 }
